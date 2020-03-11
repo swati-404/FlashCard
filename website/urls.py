b0cdf4d968bb35f680cb764ce3a51262c0de7f54
Swati Rangari <swatirangari44@gmail.com>
2020-03-11 16:23:02 +0530

    Front Pages

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('addition.html', views.addition, name="add"),
+    path('subtraction.html', views.subtraction, name="sub"),
+    path('multiplication.html', views.multiplications, name="multi"),
+    path('division.html', views.division, name="division"),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'website/home.html', {})
+
+def addition(request):
+    return render(request, 'website/addition.html', {})
+
+
+def subtraction(request):
+    return render(request, 'website/subtraction.html', {})
+
+def multiplications(request):
+    return render(request, 'website/multiplications.html', {})
+
+def division(request):
+    return render(request, 'website/division.html', {})
+
